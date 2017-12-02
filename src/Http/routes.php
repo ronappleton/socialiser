a645724e436ab791689e6f68b934fc17578cc903
Ron Appleton <ron.appleton@gmail.com>
2017-12-02 16:04:59 +0000

    Forgot to name Controllers

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,13 +1,13 @@
 <?php
 Route::group(['prefix' => 'socialiser'], function () {
     Route::group(['prefix' => 'login'], function () {
-        Route::get('{provider}', 'RonAppleton\Socialiser\Http\Controllers@socialLogin');
-        Route::get('{provider}/callback', 'RonAppleton\Socialiser\Http\Controllers@csocialCallback');
+        Route::get('{provider}', 'RonAppleton\Socialiser\Http\Controllers\LoginController@socialLogin');
+        Route::get('{provider}/callback', 'RonAppleton\Socialiser\Http\Controllers\LoginController@csocialCallback');
     });
 
     Route::group(['prefix' => 'connect'], function () {
-        Route::get('{provider}', 'RonAppleton\Socialiser\Http\Controllers@socialLogin');
-        Route::get('{provider}/callback', 'RonAppleton\Socialiser\Http\Controllers@socialCallback');
+        Route::get('{provider}', 'RonAppleton\Socialiser\Http\Controllers\ConnectController@socialLogin');
+        Route::get('{provider}/callback', 'RonAppleton\Socialiser\Http\Controllers\ConnectController@socialCallback');
     });
 });
 
