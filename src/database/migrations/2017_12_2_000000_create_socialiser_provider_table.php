9cdc4faf1622ccfbb3260fd72ebe46b182dce712
Ron Appleton <ron.appleton@gmail.com>
2017-12-03 11:27:17 +0000

    Adds migrations for socialiser_provider_users

---
+++ src/database/migrations/2017_12_2_000000_create_socialiser_provider_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocialiserProviderUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socialiser_provider_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->string('provider')->length(20);
+            $table->string('provider_id')->length(125);
+            $table->string('name')->length(125)->nullable();
+            $table->string('nickname')->length(125)->nullable();
+            $table->string('email')->length(125)->nullable();
+            $table->string('avatar')->length(125)->nullable();
+            $table->string('token')->length(125)->nullable();
+            $table->string('refresh_token')->length(125)->nullable();
+            $table->timestamp('expires_in')->nullable();
+
+            $table->unique(['provider', 'provider_id']);
+            $table->unique(['provider', 'email']);
+
+            $table->timestamps();
+        });
+
+        if(userModel())
+        {
+            Schema::table('socialiser_provider_users', function (Blueprint $table) {
+                $table->foreign('user_id')->references(userPrimaryKeyColumn())->on(userModelTable())->onUpdate('RESTRICT')->onDelete('RESTRICT');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('socialiser_provider_users');
+    }
+}
